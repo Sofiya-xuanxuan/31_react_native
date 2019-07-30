b231c3ea2aadb74d0bb79b9002f7b558ae53d239
sofiya <Sofiya.qiaoxu@ksher.net>
2019-07-30 12:44:58 +0800

    顶部导航

--- rn_1/src/Navigator/AppTopNavigator.js
+++ rn_1/src/Navigator/AppTopNavigator.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { View, Text } from 'react-native'
 import {
   createAppContainer,
   createMaterialTopTabNavigator
@@ -7,28 +8,47 @@ import {
 import HomePage from '../Pages/HomePage'
 import MyPage from '../Pages/MyPage'
 import HotPage from '../Pages/HotPage'
+import SectionListPage from '../Pages/SectionListPage'
+import SwipeableFlatListPage from '../Pages/SwipeableFlatListPage'
 
-const AppTopNavigator = createMaterialTopTabNavigator({
-  Home: {
-    screen: HomePage
+const AppTopNavigator = createMaterialTopTabNavigator(
+  {
+    Home: {
+      screen: HomePage
+    },
+    My: {
+      screen: MyPage
+    },
+    Hot: {
+      screen: HotPage
+    },
+    SwipeableFlatList: {
+      screen: SwipeableFlatListPage
+    },
+    SectionList: {
+      screen: SectionListPage
+    }
   },
-  My: {
-    screen: MyPage
-  },
-  Hot: {
-    screen: HotPage
+  {
+    lazy: true,
+    swipeEnabled: true,
+    tabBarOptions: {
+      upperCaseLabel: false,
+      scrollEnabled: true
+    },
+    initialRouteName: 'Hot'
   }
-})
+)
 
 class TopNavigator extends Component {
   render() {
-    const AppTNavigator = createAppContainer(TopNavigator)
+    const AppTNavigator = createAppContainer(AppTopNavigator)
     return (
-      <View style={{ flex: 1, marginTop: 40 }}>
+      <View style={{ flex: 1, paddingTop: 40 }}>
         <AppTNavigator />
       </View>
     )
   }
 }
 //AppNavigator不能直接暴露给根组件，所以需要使用createAppContainer包裹一下
-export default createAppContainer(TopNavigator)
+export default TopNavigator
